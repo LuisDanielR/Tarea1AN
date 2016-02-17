c9291fa97764938192c9cfc7f4fdac225701bf67
LuisDaniel <luisdaniel@LuisDaniels-MacBook-Pro.local>
2016-02-16 22:58:01 -0800

    1.0

--- Tarea1 AN/ViewController.swift
+++ Tarea1 AN/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+        
+    
+    @IBOutlet weak var resultado: UITextView!
+    
+    @IBOutlet weak var textfieldIsbn: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    @IBAction func buscar(sender: AnyObject) {
+        sincrono(self.textfieldIsbn.text)
+        
+    }
+    
+   
+   
+    @IBAction func limpiar(sender: AnyObject) {
+        self.textfieldIsbn.text = nil
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func sincrono(var x : String){
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"+x
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding)
+        
+        if texto! == "{}" {
+            self.resultado.text = "problemas con Internet"
+        }else{
+            self.resultado.text = String (texto!)
+        }
+      
+    }
+    
+  
 }
-
